d6edc8966735ae89e51a67e8b7aa992ade6e7a23
jinyun <truejinyun@gmail.com>
2024-06-18 15:51:37 +0900

    KAN-9 <feat: 테스트>

--- src/pages/EveryYawm.tsx
+++ src/pages/EveryYawm.tsx
@@ -1,7 +1,7 @@
 export default function EveryYawm() {
   return (
     <h1 className="text-5xl font-bold underline">
-      환영합니다. 김진윤 페이지입니다.
+      환영합니다. 김진윤 페이지입니다. 테스트 용으로 pr을 한 번 보내겠습니다.
     </h1>
-  );
+  )
 }
