852e61a7398254ff2b1335c29f6f8b0383fa6578
Park-Jingyeong <pjg0305pjg@gmail.com>
2024-06-19 10:07:46 +0900

    Feat: 박진경 테스트 PR

--- src/pages/ParkJingyeong.tsx
+++ src/pages/ParkJingyeong.tsx
@@ -1,7 +1,7 @@
 export default function ParkJingyeong() {
   return (
     <h1 className="text-5xl font-bold underline">
-      환영합니다. 박진경 페이지입니다.
+      환영합니다. 박진경 페이지입니다. TEST-PR
     </h1>
-  );
+  )
 }
